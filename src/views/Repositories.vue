3247c94f78e4b46501b6d1bd90ad7230ca6d7bc1
PSWai <parksoon_my@hotmail.com>
2019-01-11 22:55:50 +0800

    feat(Repositories): Add repository list view

--- src/views/Repositories.vue
+++ src/views/Repositories.vue
@@ -1,9 +1,38 @@
 <template>
-  <div>placeholder</div>
+  <div>
+    {{ $route.params.user }}'s repositories
+    <ul>
+      <li v-for="repo in repos" :key="repo.id">
+        <router-link
+          :to="{ name: 'repoDetails', params: { repoName: repo.name } }"
+          >{{ repo.name }}</router-link
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Repositories"
+  name: "Repositories",
+  data() {
+    return {
+      repos: []
+    };
+  },
+  created() {
+    this.fetchRepos();
+  },
+  watch: {
+    $route: "fetchRepos"
+  },
+  methods: {
+    fetchRepos() {
+      // TODO handle 404
+      fetch(`https://api.github.com/users/${this.$route.params.user}/repos`)
+        .then(resp => resp.json())
+        .then(payload => (this.repos = payload));
+    }
+  }
 };
 </script>
