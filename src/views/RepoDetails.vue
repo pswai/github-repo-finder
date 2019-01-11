077791fe3c93004acd577bb4ecf221d773ef4dbd
PSWai <parksoon_my@hotmail.com>
2019-01-11 22:56:05 +0800

    feat(RepoDetails): Add repo details view

--- src/views/RepoDetails.vue
+++ src/views/RepoDetails.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>placeholder</div>
+  <div>
+    {{ $route.params.repoName }}
+    <pre>
+      {{ readme }}
+    </pre>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Repositories"
+  name: "Repositories",
+  data() {
+    return {
+      readme: ""
+    };
+  },
+  created() {
+    this.fetchReadme();
+  },
+  watch: {
+    $route: "fetchReadme"
+  },
+  methods: {
+    fetchReadme() {
+      const { user, repoName } = this.$route.params;
+
+      // TODO handle 404
+      fetch(`https://api.github.com/repos/${user}/${repoName}/readme`)
+        .then(resp => resp.json())
+        .then(payload => (this.readme = atob(payload.content)));
+    }
+  }
 };
 </script>
